053d94f96d7e1029cccd67287a5b1d2de178de90
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-11-25 13:17:50 +0600

    Update: Move authentication logic into application

--- Sanri.API/Authorization.API/Controllers/SignInController.cs
+++ Sanri.API/Authorization.API/Controllers/SignInController.cs
@@ -31,40 +31,8 @@ namespace Sanri.API.Authorization.API.Controllers
         [HttpPost, NhSession]
         public async Task<ActionResult<string>> Post(SignInRequest request)
         {
-            // var command = _mapper.Map<SignInCommand>(request);
-            // return await _signInHandler.Handle(command);
-            var user = Authenticate(request);
-
-            var token = Generate(user);
-            return Ok(token);
-        }
-
-        private string Generate(SignInRequest user)
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.Username),
-                // new Claim(ClaimTypes.Email, user.EmailAddress),
-                // new Claim(ClaimTypes.GivenName, user.GivenName),
-                // new Claim(ClaimTypes.Surname, user.Surname),
-                // new Claim(ClaimTypes.Role, user.Role)
-            };
-
-            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-                                             _config["Jwt:Audience"],
-                                             claims,
-                                             expires: DateTime.Now.AddMinutes(15),
-                                             signingCredentials: credentials);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
-        private SignInRequest Authenticate(SignInRequest userLogin)
-        {
-            return userLogin;
+            var command = _mapper.Map<SignInCommand>(request);
+            return await _signInHandler.Handle(command);
         }
     }
 }
--- Sanri.Application/Authorization.API/Handlers/SignInHandler.cs
+++ Sanri.Application/Authorization.API/Handlers/SignInHandler.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Sanri.Application.Authorization.API.Repositories;
 using Sanri.Core.Models;
 
@@ -16,17 +22,26 @@ namespace Sanri.Application.Authorization.API.Handlers
     {
         private readonly UserRepository _userRepository;
         private readonly PasswordHasher<User> _passwordHasher;
+        private readonly IConfiguration _config;
 
-        public SignInHandler(UserRepository userRepository, PasswordHasher<User> passwordHasher)
+        public SignInHandler(UserRepository userRepository, PasswordHasher<User> passwordHasher, IConfiguration config)
         {
             _userRepository = userRepository;
             _passwordHasher = passwordHasher;
+            _config    = config;
         }
 
         public async Task<string> Handle(SignInCommand command)
         {
             var user      = await _userRepository.GetSingle(command.Username);
             var isCorrect = VerifyPassword(user, user.Password, command.Password);
+
+            if (isCorrect)
+            {
+                // var user = Authenticate(command);
+                var token = Generate(user);
+                return token;
+            }
 
             return user.Username;
         }
@@ -36,6 +51,34 @@ namespace Sanri.Application.Authorization.API.Handlers
             var isCorrect = _passwordHasher.VerifyHashedPassword(user, hashedPassword, givenPassword);
 
             return isCorrect == PasswordVerificationResult.Success;
+        }
+        
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                // new Claim(ClaimTypes.Email, user.EmailAddress),
+                // new Claim(ClaimTypes.GivenName, user.GivenName),
+                // new Claim(ClaimTypes.Surname, user.Surname),
+                // new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                                             _config["Jwt:Audience"],
+                                             claims,
+                                             expires: DateTime.Now.AddMinutes(15),
+                                             signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private SignInCommand Authenticate(SignInCommand userLogin)
+        {
+            return userLogin;
         }
     }
 }
