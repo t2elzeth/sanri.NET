ede824fdd61726cd775204be2dd9769cf1de06ff
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-12-30 20:09:26 +0600

    Client.cs

---
+++ Sanri.Core.Tests/ClientBalanceTests.cs
@@ -0,0 +1,37 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Sanri.Core.Tests;
+
+public class ClientBalanceTests
+{
+    [Fact]
+    public void It_should_withdraw_if_enough()
+    {
+        var client = Client.Create(balance: 20);
+
+        client.WithdrawBalance(10);
+
+        client.Balance.Should().Be(10);
+    }
+
+    [Fact]
+    public void It_should_withdraw_if_not_enough()
+    {
+        var client = Client.Create(balance: 20);
+
+        client.WithdrawBalance(30);
+
+        client.Balance.Should().Be(-10);
+    }
+
+    [Fact]
+    public void It_should_replenish()
+    {
+        var client = Client.Create(balance: 20);
+
+        client.ReplenishBalance(10);
+
+        client.Balance.Should().Be(30);
+    }
+}
--- Sanri.Core/Client.cs
+++ Sanri.Core/Client.cs
@@ -2,5 +2,27 @@ namespace Sanri.Core;
 
 public class Client
 {
-    
+    public long Balance { get; private set; }
+
+    private Client()
+    {
+    }
+
+    public static Client Create(long balance = 0)
+    {
+        return new Client
+        {
+            Balance = balance
+        };
+    }
+
+    public void WithdrawBalance(long amount)
+    {
+        Balance -= amount;
+    }
+
+    public void ReplenishBalance(long amount)
+    {
+        Balance += amount;
+    }
 }
